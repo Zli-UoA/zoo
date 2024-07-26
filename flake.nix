1e403a0772fe268385d4c95d2c061d40621e352c
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2024-07-27 00:50:14 +0900

    add nix files for flake users

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Zoo - Zli cOmmunication tOol";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
+        };
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.stdenv.mkDerivation {
+          name = "rust-env";
+          nativeBuildInputs = with pkgs; [
+            toolchain
+            gnumake
+            nodejs_22
+          ];
+        };
+      }
+    );
+}
