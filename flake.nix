8a728d893124d3401f4c0a091a9a634bb1989969
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2024-08-18 04:05:27 +0900

    change flake to fix prisma issue

--- flake.nix
+++ flake.nix
@@ -27,7 +27,17 @@
             toolchain
             gnumake
             nodejs_22
+            nodePackages.prisma
+            openssl
+            prisma-engines
           ];
+          shellHook = with pkgs; ''
+            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          '';
         };
       }
     );
