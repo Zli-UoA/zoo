aa9f740cafd296fd85f51b148e54fd361c6da1d0
noharu36 <todofamily_h@icloud.com>
2025-03-26 07:35:08 +0900

    fix

--- backend/src/usecase/channel.rs
+++ backend/src/usecase/channel.rs
@@ -108,11 +108,11 @@ mod test {
                         "%Y-%m-%d %H:%M:%S",
                     )
                     .unwrap(),
-                    updated_at: DateTime::parse_from_str(
+                    updated_at: Some(DateTime::parse_from_str(
                         "2024-08-08 00:00:00",
                         "%Y-%m-%d %H:%M:%S",
                     )
-                    .unwrap(),
+                    .unwrap()),
                 },
             )]])
             .into_connection();
