a79a9351fd65100cc2740b7bdc77098a136910dc
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2025-03-27 05:07:11 +0900

    fix rust env

--- flake.nix
+++ flake.nix
@@ -29,7 +29,8 @@
             gnumake
             nodejs_22
             nodePackages.prisma
-            openssl
+            openssl.dev
+            pkg-config
             prisma-engines
           ];
           shellHook = with pkgs; ''
