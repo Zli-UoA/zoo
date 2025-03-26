96022a66465f1183db4dadbfb16eb865d3c50e10
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2025-03-27 05:07:29 +0900

    feat: add get all users

--- backend/src/handler/user.rs
+++ backend/src/handler/user.rs
@@ -8,6 +8,7 @@ use crate::usecase;
 
 #[derive(Default)]
 pub struct UserQuery;
+pub struct UserListQuery;
 
 #[Object]
 impl UserQuery {
@@ -21,6 +22,21 @@ impl UserQuery {
         match usecase::user::get_user_by_id(ctx, id.as_str()).await {
             Ok(Some(user)) => Ok(user),
             Ok(None) => Err(Error::new("Not found")),
+            Err(_) => Err(Error::new("Internal server error")),
+        }
+    }
+}
+
+#[Object]
+impl UserListQuery {
+    async fn users<'ctx>(
+        &self,
+        ctx: &async_graphql::Context<'ctx>,
+    ) -> Result<Vec<models::user::User>> {
+        let ctx = ctx.data_unchecked::<Context>();
+
+        match usecase::user::get_all_users(ctx).await {
+            Ok(users) => Ok(users),
             Err(_) => Err(Error::new("Internal server error")),
         }
     }
