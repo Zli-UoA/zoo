74563f339e6973b3ffbf1f5492775c9d1e5ccdbe
noharu36 <todofamily_h@icloud.com>
2025-08-21 11:42:57 +0900

    testの軽い修正と、チャンネル取得機能の実装

--- backend/src/usecase/channel.rs
+++ backend/src/usecase/channel.rs
@@ -1,24 +1,92 @@
+use async_graphql::{Error, Result};
+use sea_orm::{EntityTrait, ModelTrait};
+
 use crate::context::Context;
-use crate::models::channel::Channel;
+use crate::generate::entities::user::{self, Entity as Owner};
+use crate::generate::entities::channel_user::Entity as Member;
+use crate::models::{channel::Channel, user::User};
 
-pub async fn get_all_channel(_ctx: &Context) -> Result<Vec<Channel>, ()> {
-    todo!("generate::entities::channel::Model to models::channel::Channel")
+use crate::generate::entities::channel;
+
+pub async fn get_all_channel(ctx: &Context) -> Result<Vec<Channel>, String> {
+    let Ok(channel) = channel::Entity::find().all(&ctx.db).await else {
+        return Err("DB error".to_string());
+    };
+
+    Ok(channel
+        .into_iter()
+        .map(|channel| Channel {
+            id: channel.id,
+            name: channel.channel_name,
+            description: channel.description,
+            created_at: channel.created_at.to_string(),
+            archived: channel.archive_at.is_some(),
+            private: channel.is_private,
+        })
+        .collect())
 }
 
-// User型を返すように要修正
 pub async fn get_channel_owner_by_channel_id(
-    _ctx: &Context,
-    _channel_id: &str,
-) -> Result<String, ()> {
-    todo!()
+    ctx: &Context,
+    channel_id: &str,
+) -> Result<User, String> {
+    let Ok(Some(channel)) = channel::Entity::find_by_id(channel_id).one(&ctx.db).await else {
+        return Err("DB error. channel could not be found".to_string());
+    };
+
+    let Ok(owner) = channel.find_related(Owner).one(&ctx.db).await else {
+        return Err("DB error".to_string());
+    };
+
+    if let Some(owner) = owner {
+        Ok(User {
+            id: owner.id,
+            name: owner.user_name,
+            display_name: owner.display_name,
+        })
+    } else {
+        Err("owner could not be found.".to_string())
+    }
 }
 
-// User型を返すように要修正
 pub async fn get_channel_users_by_channel_id(
-    _ctx: &Context,
-    _channel_id: &str,
-) -> Result<Option<Vec<String>>, ()> {
-    todo!()
+    ctx: &Context,
+    channel_id: &str,
+) -> Result<Vec<Option<User>>, String> {
+    let Ok(Some(channel)) = channel::Entity::find_by_id(channel_id).one(&ctx.db).await else {
+        return Err("DB error. channel could not be found".to_string());
+    };
+
+    let Ok(members) = channel.find_related(Member).all(&ctx.db).await else {
+        return Err("DB error".to_string());
+    };
+
+
+    let mut errors = Vec::new();
+    let futures = members.into_iter().map(async |member| {
+        get_user_by_channel_user(ctx, &member.user_id).await
+    });
+    let results = futures::future::join_all(futures).await;
+    let users: Vec<Option<User>> = results.into_iter()
+        .filter_map(|r| r.map_err(|e| errors.push(e)).ok())
+        .collect::<Vec<_>>();
+    if !errors.is_empty() {
+        return Err(format!("failed get user: {:?}", errors))
+    } else {
+        Ok(users)
+    }
+}
+
+async fn get_user_by_channel_user(ctx: &Context, id: &str) -> Result<Option<User>, String> {
+    let Ok(user) = user::Entity::find_by_id(id).one(&ctx.db).await else {
+        return Err("DB error".to_string());
+    };
+
+    Ok(user.map(|user| User {
+        id: user.id,
+        name: user.user_name,
+        display_name: user.display_name,
+    }))
 }
 
 // Message型を返すように要修正
@@ -36,6 +104,7 @@ mod test {
     use crate::generate::entities::channel;
     use crate::generate::entities::user;
     use crate::models::channel::Channel;
+    use crate::models::user::User;
     use sea_orm::prelude::*;
     use sea_orm::MockDatabase;
 
@@ -83,37 +152,28 @@ mod test {
     async fn チャンネルオーナーを取得する() {
         // Arrange
         let db: DatabaseConnection = MockDatabase::new(sea_orm::DatabaseBackend::Postgres)
-            .append_query_results([vec![(
-                channel::Model {
-                    id: "0".to_string(),
-                    channel_name: "hoge".to_string(),
-                    description: Some("huga".to_string()),
-                    is_private: false,
-                    created_user_id: "aaa".to_string(),
-                    created_at: DateTime::parse_from_str(
-                        "2024-08-08 00:00:00",
-                        "%Y-%m-%d %H:%M:%S",
-                    )
+            .append_query_results([vec![channel::Model {
+                id: "0".to_string(),
+                channel_name: "hoge".to_string(),
+                description: Some("huga".to_string()),
+                is_private: false,
+                created_user_id: "aaa".to_string(),
+                created_at: DateTime::parse_from_str("2024-08-08 00:00:00", "%Y-%m-%d %H:%M:%S")
                     .unwrap(),
-                    updated_at: None,
-                    archive_at: None,
-                    deleted_at: None,
-                },
-                user::Model {
-                    id: "aaa".to_string(),
-                    user_name: "haru".to_string(),
-                    display_name: "haru".to_string(),
-                    created_at: DateTime::parse_from_str(
-                        "2024-08-08 00:00:00",
-                        "%Y-%m-%d %H:%M:%S",
-                    )
+                updated_at: None,
+                archive_at: None,
+                deleted_at: None,
+            }]])
+            .append_query_results([vec![user::Model {
+                id: "aaa".to_string(),
+                user_name: "haru".to_string(),
+                display_name: "haru".to_string(),
+                created_at: DateTime::parse_from_str("2024-08-08 00:00:00", "%Y-%m-%d %H:%M:%S")
                     .unwrap(),
-                    updated_at: Some(
-                        DateTime::parse_from_str("2024-08-08 00:00:00", "%Y-%m-%d %H:%M:%S")
-                            .unwrap(),
-                    ),
-                },
-            )]])
+                updated_at: Some(
+                    DateTime::parse_from_str("2024-08-08 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap(),
+                ),
+            }]])
             .into_connection();
 
         let context = Context {
@@ -122,11 +182,18 @@ mod test {
         };
 
         // Action
-        let result = get_channel_owner_by_channel_id(&context, "aaa")
+        let result = get_channel_owner_by_channel_id(&context, "0")
             .await
             .unwrap();
         // Assert
-        assert_eq!(result, "aaa".to_string())
+        assert_eq!(
+            result,
+            User {
+                id: "aaa".to_string(),
+                name: "haru".to_string(),
+                display_name: "haru".to_string()
+            }
+        )
     }
 
     #[tokio::test]
